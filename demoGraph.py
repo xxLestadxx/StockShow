5d07f3f654e67c5e823aeb9614784c50e160d535
lestad <j.v.gospodinov@gmail.com>
2021-07-13 15:41:56 +0300

    update

--- demoGraph.py
+++ demoGraph.py
@@ -10,45 +10,43 @@ client = InfluxDBClient(url="http://localhost:8086", token="CKcxAqymiBzP7siL6MqN
 write_api = client.write_api(write_options=SYNCHRONOUS )
 query_api = client.query_api()
 
-i = 1
-while i < 1000:
+
+while True:
   #Temperature
   randomNum = random.randrange(20,30) + random.uniform(0,1)
   p = Point("Building").tag("location", "Prague").field("temperature", randomNum)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit temp ", randomNum)
+  print(" opit temp ", randomNum)
   randomNum = random.randrange(20,30) + random.uniform(0,1)
   p = Point("Building").tag("location", "Varna").field("temperature", randomNum-5)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit temp ", randomNum)
+  print(" opit temp ", randomNum)
   
   #PPM
   randomNum = random.randrange(900,1100) + random.uniform(0,1)
   p = Point("Building").tag("location", "Prague").field("parts_per_notation", randomNum)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit ", randomNum)
+  print(" opit ppm ", randomNum)
   randomNum = random.randrange(800,1000) + random.uniform(0,1)
   p = Point("Building").tag("location", "Varna").field("parts_per_notation", randomNum)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit ", randomNum)
+  print(" opit  ppm ", randomNum)
   
 
   #electricity consumption
   randomNum = random.randrange(3,8) + random.uniform(0,1)
   p = Point("Building").tag("location", "Prague").field("electricity_consumption", randomNum)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit ", randomNum)
+  print(" opit el cons ", randomNum)
   
 
   #electricity efficiency 
   randomNum = random.randrange(3,8) + random.uniform(0,1)
   p = Point("Building").tag("location", "Prague").field("electricity_consumption_efficiency", randomNum)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit ", randomNum)
+  print( " opit el eff ", randomNum)
   randomNum = random.randrange(1,5) + random.uniform(0,1)
   p = Point("Building").tag("location", "Varna").field("electricity_consumption_efficiency", randomNum)
   write_api.write(bucket=bucket, record=p)
-  print(i, " opit ", randomNum)
+  print(" opit el eff ", randomNum)
   time.sleep(60)
-
-  i += 1
